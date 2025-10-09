157610893936d3e3849a5d4cb3550557f32cddb3
santhosh <santhoshs0305@gmail.com>
2025-10-09 15:28:08 -0400

    fixed eslint error

--- src/components/ProjectTiles.js
+++ src/components/ProjectTiles.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import projectData from '../data/projects.json';
 
 const ProjectTiles = ({ onProjectClick }) => {
