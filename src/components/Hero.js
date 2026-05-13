1391bdf0f6b3d33ac6031525ed9ef1e1424a8a0c
SanthoshS0305 <santhoshs0305@gmail.com>
2026-05-13 16:14:36 -0400

    updated hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -101,10 +101,10 @@ const Hero = () => {
           </div>
           <div className="hero-about">
             <p>
-              I'm a Computer Science Student Minoring in Writing and Rhetoric at Stony Brook University in New York. I'm also a professional content creator and writer.
+              I'm a Computer Science Student Minoring in Writing and Rhetoric at Stony Brook University in New York. I'm also a professional content creator and writer. I am currently an <strong>Assistant Digital Marketing Coordinator for Thump Local</strong>, and a <strong>research assistant for PoliTech, under Professor Robert Kelly</strong>.
             </p>
             <p>
-              I am passionate about computers and people. That is why I am Vice President of the Stony Brook University Game Development and Design Club and the Public Relations Officer of the Stony Brook Computing Society.
+              I am passionate about computers and people. That is why I am a <strong>Peer Mentor</strong> for the <strong>College of Engineering and Applied Sciences' Peer Mentoring Program</strong>. I was also the <strong>Vice President of the SBU Game Development and Design Club</strong> and the <strong>Public Relations Officer for the Stony Brook Computing Society</strong>.
             </p>
             <p>
               As an avid artist, I love creating{' '}
