97be0855844c5c6e268a0104b8445ec5037d5f60
SanthoshS0305 <santhoshs0305@gmail.com>
2026-05-13 15:46:08 -0400

    feat: add Substack link to My Other Platforms section

--- src/components/About.js
+++ src/components/About.js
@@ -87,6 +87,12 @@ const About = () => {
               </svg>
               Instagram
             </a>
+            <a href="https://dashesnothyphens.substack.com" target="_blank" rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11 22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z"/>
+              </svg>
+              Substack
+            </a>
           </div>
         </div>
       </div>
